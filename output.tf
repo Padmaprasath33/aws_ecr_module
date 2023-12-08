fe29fd475f13d50057fe69c04207c1755d973da2
Padmaprasath <padmaprasath33@gmail.com>
2023-12-08 15:10:42 +0530

    Added ECS config

--- output.tf
+++ output.tf
@@ -1,3 +1,7 @@
-output "cohort_demo_ecr_repo_url" {
-  value = aws_ecr_repository.cohort_demo.repository_url
+output "cohort_demo_ui_ecr_repo_url" {
+  value = aws_ecr_repository.cohort_demo_ui.repository_url
+}
+
+output "cohort_demo_backend_ecr_repo_url" {
+  value = aws_ecr_repository.cohort_demo_backend.repository_url
 }
